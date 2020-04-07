99954495daee1bffb32cd2af6e5929e87c132387
marcos <eptaccio@gmail.com>
2020-04-06 23:45:55 -0300

    add tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        // trackingId: 'ADD YOUR TRACKING ID HERE',
+        trackingId: 'UA-113158157-1'
       }
     },
     'gatsby-plugin-feed',
